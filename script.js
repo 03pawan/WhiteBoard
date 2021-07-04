1629a09c45e23b534930927ce8b7aa03fccbfbae
priyanshusingh305 <priyanshusingh.cs19@mpgi.edu.in>
2021-07-04 20:30:56 +0530

    third

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ let {x, y} = position;
 let fontColor = "#FF0000";
 let boardColor = "#bec2c1";
 let width = 5;
+let eraserWidth = 10;
 
 const offsetHeight = document.querySelector('.navbar').clientHeight;
 const offsetWidth = document.querySelector('#menu').clientWidth;
+
+const canvas
+
 // const offsetHeight = 30;
 // const offsetWidth = 40;
 
+
 function resize(){
-    ctx.canvas.height = window.innerHeight - offsetHeight-10;
-    ctx.canvas.width = window.innerWidth - offsetWidth-10;
+    ctx.canvas.height = window.innerHeight - offsetHeight-2;
+    ctx.canvas.width = window.innerWidth - offsetWidth-2;
     ctx.fillStyle = boardColor;
     ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
 }
@@ -29,6 +34,7 @@ canvasEl.addEventListener('mousemove', draw);
 function setPos(event){
     x = event.clientX - offsetWidth;
     y = event.clientY - offsetHeight;
+    console.log(x, y);
 }
 
 function draw(event){
@@ -46,4 +52,27 @@ function draw(event){
     ctx.closePath();
 }
 
+function eraser(event){
+    ctx.beginPath();
+    setPos(event)
+    ctx.arc(x, y, eraserWidth, 0, Math.PI*2);
+    ctx.fillStyle = boardColor;
+    ctx.strokeStyle = "black";
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+    ctx.clearRect(0,0,)
+}
 
+canvasEl.addEventListener('mousemove', eraser);
+
+// function eraser(event){
+//     ctx.beginPath();
+//     setPos(event)
+//     ctx.arc(x, y, eraserWidth, 0, Math.PI*2);
+//     ctx.fillStyle = boardColor;
+//     ctx.strokeStyle = "black";
+//     ctx.fill();
+//     ctx.stroke();
+//     ctx.closePath();
+// }
